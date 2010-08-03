44b896c31a360982fd228145e26b7caf671f7e83
Andre Andrade <andre.seduc@gmail.com>
2010-08-03 10:51:13 -0700

    changed recursive to [-1]

--- app/app_model.php
+++ app/app_model.php
@@ -1,6 +1,7 @@
 <?php
 class AppModel extends Model 
 {
+        public $recursive = -1;
 	public function __construct($id = false, $table = null, $ds = null) 
 	{
 		if ( !isset($_SERVER['HTTP_HOST']) || preg_match('/^(localhost)?$/', $_SERVER['HTTP_HOST']) )
